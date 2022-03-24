b54d3a09b90ec3bb3420c315aaa2b7f2900f0df6
Melih Omaç <37351493+Melihomac@users.noreply.github.com>
2022-03-24 12:30:18 +0300

    Add files via upload

---
+++ package1/src/main.java
@@ -0,0 +1,38 @@
+package package1;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class main extends JPanel {
+    public Color Main() {
+        int red = (int) (Math.random() * 256);
+        int green = (int) (Math.random() * 256);
+        int blue = (int) (Math.random() * 256);
+        return (new Color(red,green,blue));
+    }
+    public class MouseListener extends MouseAdapter {
+        @Override
+        public void mouseClicked (MouseEvent e) {
+            setBackground(Main());
+        }
+    }
+    public main() {
+        setBackground(Main());
+        addMouseListener(new MouseListener());
+    }
+    public static void main (String args []) {
+        JFrame gui = new JFrame();
+        gui.setTitle("Rastgele Renkler");
+        gui.setSize(300,300);
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container pane = gui.getContentPane();
+        pane.setLayout(new GridLayout(5,5));
+        for (int x= 1; x<=25; x++) {
+            main m = new main();
+            pane.add(m);
+        }
+        gui.setVisible(true);
+    }
+}
